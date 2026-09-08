bb0c8616c527519679800d44fa5e4c4c9d84253b
Maksim Shyshko <lamakswork@gmail.com>
2026-09-08 11:20:45 +0300

    Add OrdersView

--- ios/FastFood/FastFood/ContentView.swift
+++ ios/FastFood/FastFood/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let orderService: OrderServiceProtocol = MockOrderService()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        OrdersView(orderService: orderService)
     }
 }
 
---
+++ ios/FastFood/FastFood/Views/OrdersView.swift
@@ -0,0 +1,105 @@
+//
+//  OrdersView.swift
+//  FastFood
+//
+//  Created by Maksim Shyshko on 08.09.2026.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct OrdersView: View {
+
+    let orderService: OrderServiceProtocol
+
+    @State private var readyOrders: [ShortOrder] = []
+    @State private var inProgressOrders: [ShortOrder] = []
+
+    var body: some View {
+        HStack {
+            VStack {
+                VStack {
+                    Title(title: "Ready orders")
+                    OrdersList(orders: $readyOrders)
+                }
+                .padding([.leading, .trailing])
+                Spacer()
+            }
+            .background(
+                Rectangle()
+                    .foregroundStyle(.blue)
+            )
+
+            VStack {
+                VStack {
+                    Title(title: "In progress")
+                    OrdersList(orders: $inProgressOrders)
+
+                }
+                .padding([.leading, .trailing])
+                Spacer()
+
+            }
+            .background(
+                Rectangle()
+                    .foregroundStyle(.green)
+            )
+        }
+        .ignoresSafeArea()
+        .task {
+            await loadOrders()
+        }
+    }
+
+    private func loadOrders() async {
+            do {
+                let fetchedOrders = try await orderService.getOrders()
+                readyOrders = fetchedOrders.ready
+                inProgressOrders = fetchedOrders.notReady
+            } catch {
+                print("Ошибка при загрузке заказов: \(error)")
+            }
+    }
+
+}
+
+struct Title: View {
+    let title: String
+
+    var body: some View {
+        HStack {
+            Spacer()
+            Text(title)
+                .font(.largeTitle)
+                .bold()
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct OrdersList: View {
+
+    @Binding var orders: [ShortOrder]
+
+    var body: some View {
+        ForEach(orders, id: \.id) { order in
+            HStack {
+                Text(String(order.id))
+                    .font(.largeTitle)
+                    .bold()
+                    .padding([.top, .leading, .trailing])
+                Spacer()
+                Text(order.customerName)
+                    .font(.largeTitle)
+                    .bold()
+                    .padding([.top, .leading, .trailing])
+            }
+        }
+    }
+}
+
+#Preview {
+    OrdersView(orderService: MockOrderService())
+}
